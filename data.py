9de38917634ad88dc1074740b21a85a26c940c2b
melody.yu <melody.yu12@gmail.com>
2023-09-11 16:22:16 -0700

    Create df with script data

--- data.py
+++ data.py
@@ -1,2 +1,9 @@
 import pandas as pd
+import sele
 
+
+#Create dF with column names
+# df = pd.DataFrame(columns=['Name', 'Igloo Price', 'Jellyneo Price', 'Category'])
+df = pd.DataFrame(sele.data)
+
+print(df)
