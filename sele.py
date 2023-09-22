205e62db9723eb0c66a90791608ea5e6ea914718
melody.yu <melody.yu12@gmail.com>
2023-09-21 23:16:44 -0700

    Temp dupe function
    Added in function to avoid adding in duplicates to the inventory.txt file. There do be a bug where it's adding too many times so working on that atm

--- sele.py
+++ sele.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.by import By
 import time
 import re 
 
+from collections import OrderedDict
     
 #open selenium browser 
 options = webdriver.ChromeOptions()
@@ -46,7 +47,7 @@ print("Entered igloo")
 
 counter = 0
 total_time = 0
-for i in range(5):
+for i in range(2):
     start = time.time()
 
     #get item information
@@ -78,11 +79,105 @@ for i in range(5):
         print("GREAT SIZE: ", len(inventory))
 
         #outputs item name,cost to .txt file
-        with open("inventory.txt", "a") as file:
-            inv_dict = dict(zip(inventory, item_costs))
-            for key,value in inv_dict.items():
-                file.write(key+":"+value+"\n")
+        # with open("inventory.txt", "a+") as file:
+        #     #set file pointer to top of file (it defaults at end)
+
+    
+ 
+        inv_dict = OrderedDict(zip(inventory, item_costs)) #create dictionary from the two lists
+        #     #write inventory as name:price pairs
+        #     for key,value in inv_dict.items():
+        #         file.seek(0)
+        #         file_line = file.read().splitlines()
+        #         print(file_line)
+
+        #         if key not in file_line:
+        #             file.write(key+":"+value+"\n")
+        #         else:
+        #             pass
+        # file.close()
+        # print(inv_dict)
+
+
+        # for key,value in inv_dict.items():
+        #     with open("inventory.txt", "a+") as file:
+
+        #         file.seek(0)
+        #         file_line = file.read().splitlines()
+        #         # print(file_line)
+
+        #         print(len(file_line))
+                
+        #         # for line in file_line: 
+                        
+        #         #     if key not in file_line:
+        #         #         print(key, value)
+        #         #         file.write(key+":"+value+"\n")
+
+        #             # print("already in")
+        #         #     pass
+        #         # else: 
+        #         #     file.write(key+":"+value+"\n")
+        #     file.close()
+        # print(inv_dict)
+
+        with open("inventory.txt", "a+") as file:
+            file.seek(0)
+            file_line = file.read().splitlines()
+            # print("this is the file line (size: ", len(file_line), "): \n", file_line)
+
+            # for i in file_line:
+            #     print(i)
+            #if file is empty, append entire inventory list to it
+            if len(file_line) == 0:
+                print("Nothing in file so far")
+                for key, value in inv_dict.items():
+                    file.write(key+":"+value+"\n")
+
+
+            # else:
+            #     file_line = dict(file_line)
+            #     for key, value in inv_dict:
+            #         if key not in file_line: 
+            #             file.write(key+":"+value+"\n")
+            #             print("wrote something, SPECIFICALLY: {}:{}".format(key, value))
+
+
+            # #if file is not empty, check name against existing entries; add only if no dupes           
+            # else: 
+                print("im in here")
+                for key,value in inv_dict.items():
+                    for i in range(len(file_line)):
+ 
+                        print("compared {} and {}, round:{}".format(key, file_line[i],i))
+                        if key not in file_line[i]:
+                            print("wrote something, SPECIFICALLY: {}:{}".format(key, value))
+                            file.write(key+":"+value+"\n")
+                            # i+=1
+                            # break #--->gets stuck on round1 max. without it, checks for dupe, but then proceeds to add it like len(file_line-1) times to inventory file
+                        else:
+                            print("BROKE OUT BC OF {} and {}".format(key, file_line[i]))
+                            # i+=1
+                            # break
+                        i+=1
+                        # break #--> doesnt increment rounds at all
+                    # break
+                        
+                # result = any(key in file_line for key, value in inv_dict.items())
+                # print(result)
+                # if result == False: #key does not exist in txt file
+                #     file.write(key+":"+value+"\n")       
+                #     print("wrote something, specifically:", key+":"+value)
+
+                # for line in file_line:
+                #     for key, value in inv_dict.items():      
+                #         print(type(key), type(file_line))       
+                #         result = any(key in file_line for key in inv_dict.items())       
+                #         if result == False:
+                #             print("NOT IN:", key, value)
+                #             file.write(key+":"+value+"\n")
         file.close()
+        print("FILE IS THIS LONG:{}, PAGE INVENTORY IS THIS LONG:{}".format(len(file_line), len(inv_dict)))
         print(inv_dict)
 
     else:
